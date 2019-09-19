192431532701a483608b8531e75eec93850d75d9
jonathanhovland <jonathan.hovland@yahoo.com>
2019-09-19 10:18:24 -0600

    Created fetch route for product id in details

--- src/components/ProductDetail/index.js
+++ src/components/ProductDetail/index.js
@@ -7,12 +7,24 @@ class ProductDetail extends Component {
 
     // fetch data from ONE product
     componentDidMount() {
+        const id = this.props.match.params.id
         
+        fetch(`http://localhost:4000/products/${id}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                this.setState({
+                    product: data.product
+                })
+            })
+            .catch(error => {
+                console.log("somethings wrong darn it")
+            })
     }
 
     render() {
         return(
-            <h2>upside down</h2>
+            <h2>Product ID: {this.state.product.id} </h2>
         )
     }
 }
