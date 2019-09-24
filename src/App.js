8dd4c6454b28c2ec748852b13c0447076fe99584
jonathanhovland <jonathan.hovland@yahoo.com>
2019-09-24 08:54:00 -0600

    created a payment success component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from "./components/Home"
 import Navigation from "./components/Navigation"
 import ProductsList from "./components/ProductsList"
 import ProductDetail from "./components/ProductDetail"
+import PaymentSuccess from "./components/PaymentSuccess"
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/products" component={ProductsList} />
         <Route path="/products/:id" component={ProductDetail}/>
+        <Route path="/success" component={PaymentSuccess}/>
       </Switch>
     </Router>
   )
